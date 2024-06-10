9594615195f8fe03a3f1fddfbbc9afb4ae562439
Paul Llanque <llanquepol@gmail.com>
2024-06-09 22:32:59 -0500

    [update]  userModule and AuthModule is imported on appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
