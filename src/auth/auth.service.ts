6aa8fe2fdfb9a4403e34a716674675835fa4563f
Paul Llanque <llanquepol@gmail.com>
2024-06-11 00:44:57 -0500

    [update] return user and token to autenticate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
-import { User } from 'src/users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from 'src/users/user.service';
 
 import { RegisterAuthDto, LoginAuthDto } from './dto';
-import { UserService } from 'src/users/user.service';
+import { IAuthResponse, IJwtPayload } from './interfaces';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login(loginAuthDto: LoginAuthDto): Promise<User> {
+  async login(loginAuthDto: LoginAuthDto): Promise<IAuthResponse> {
     const { email, password } = loginAuthDto;
-    return await this.userService.getUserByEmail(email);
+    const user = await this.userService.getUserByEmail(email);
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Credentials are not valid (Password)');
+    }
+
+    delete user.password;
+
+    return {
+      user,
+      token: this.getJwtToken({ id: user.id }),
+    };
   }
 
-  async register(registerAuthDto: RegisterAuthDto): Promise<User> {
-    return await this.userService.create(registerAuthDto);
+  async register(registerAuthDto: RegisterAuthDto): Promise<IAuthResponse> {
+    const { password, ...rest } = registerAuthDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userData = {
+      ...rest,
+      password: hashedPassword,
+    };
+    const user = await this.userService.create(userData);
+    delete user.password;
+
+    return {
+      user,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
+  private getJwtToken(payload: IJwtPayload) {
+    return this.jwtService.sign(payload);
   }
 }
