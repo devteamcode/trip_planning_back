d768d1215889324e972975a1a0b2bcd4767d0ac7
Paul Llanque <llanquepol@gmail.com>
2024-06-10 23:37:48 -0500

    [update] user service: crud

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,43 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { RegisterAuthDto } from 'src/auth/dto';
 
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import { RegisterAuthDto } from 'src/auth/dto';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   async create(registerAuthDto: RegisterAuthDto): Promise<User> {
-    return {
-      id: 1,
-      name: registerAuthDto.name,
-      email: registerAuthDto.email,
-      password: registerAuthDto.password,
-    };
+    try {
+      return await this.userRepository.save(registerAuthDto);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new BadRequestException(error.detail.replace(/Key \(.*?\)=/, ''));
+      }
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   async getUserByEmail(email: string): Promise<User> {
-    return {
-      id: 1,
-      name: 'test',
-      email,
-      password: 'test',
-    };
+    const user = await this.userRepository.findOne({
+      where: { email, active: true },
+    });
+    if (!user)
+      throw new NotFoundException(
+        `User with email ${email} not found. Consult with an administrator.`,
+      );
+    return user;
   }
 
   async findAll(): Promise<User[]> {
-    return [];
+    return await this.userRepository.find({ where: { active: true } });
   }
 
   async findOne(id: number): Promise<User> {
-    throw new Error('Method not implemented.');
+    const user = await this.userRepository.findOne({
+      where: { id, active: true },
+    });
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    return user;
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
-    throw new Error('Method not implemented.');
+    const user = await this.findOne(id);
+    Object.assign(user, updateUserDto);
+    return await this.userRepository.save(user);
   }
 
-  async remove(id: number): Promise<User> {
-    //todo: add soft delete
-    throw new Error('Method not implemented.');
+  async remove(id: number): Promise<boolean> {
+    const resp = await this.userRepository.update({ id }, { active: false });
+    if (!resp.affected)
+      throw new NotFoundException(`User with id ${id} not found`);
+    return true;
   }
 }
