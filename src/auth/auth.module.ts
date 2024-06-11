1bb3ec1e59c01b347cd63314ef39dcf6b1867a0b
Paul Llanque <llanquepol@gmail.com>
2024-06-11 00:37:57 -0500

    [update] AuthModule: register secrect password and expire

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,24 @@ import { UserModule } from 'src/users/user.module';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { envs } from 'src/config';
 
 @Module({
-  controllers: [AuthController],
+  imports: [
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secret: envs.jwtSecret,
+        signOptions: {
+          expiresIn: `${envs.jwtExpires}h`,
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
-  imports: [UserModule],
+  controllers: [AuthController],
 })
 export class AuthModule {}
